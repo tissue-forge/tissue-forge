2ea3552b4e4e2ed4372aad8d0f0e3b18686f12e2
tjsego <tjsego@gmail.com>
2022-08-30 08:40:14 -0400

    Added edge tension mesh actor

---
+++ source/models/vertex/solver/actors/tfEdgeTension.cpp
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * This file is part of Tissue Forge.
+ * Copyright (c) 2022 T.J. Sego and Tien Comlekoglu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+
+#include "tfEdgeTension.h"
+
+#include <models/vertex/solver/tfSurface.h>
+#include <models/vertex/solver/tfVertex.h>
+
+#include <tf_metrics.h>
+
+
+using namespace TissueForge;
+using namespace TissueForge::models::vertex;
+
+
+HRESULT EdgeTension::energy(MeshObj *source, MeshObj *target, float &e) {
+    Surface *s = (Surface*)source;
+    Vertex *v = (Vertex*)target;
+
+    std::vector<Vertex*> vertices = s->getVertices();
+    std::vector<Vertex*>::iterator vc_itr = std::find(vertices.begin(), vertices.end(), v);
+    if(vc_itr == vertices.end()) 
+        return S_OK;
+
+    Vertex *vn = vc_itr == vertices.begin()   ? *(vertices.end() - 1) : *(vc_itr - 1);
+    Vertex *vp = vc_itr == vertices.end() - 1 ? *(vertices.begin())   : *(vc_itr + 1);
+
+    FVector3 posc = v->getPosition();
+    
+    FVector3 rel_pos = metrics::relativePosition(vp->getPosition(), posc);
+    float _e = rel_pos.dot(rel_pos);
+
+    rel_pos = metrics::relativePosition(posc, vn->getPosition());
+    _e += rel_pos.dot(rel_pos);
+
+    e += lam * _e;
+    return S_OK;
+}
+
+HRESULT EdgeTension::force(MeshObj *source, MeshObj *target, float *f) {
+    Surface *s = (Surface*)source;
+    Vertex *v = (Vertex*)target;
+
+    std::vector<Vertex*> vertices = s->getVertices();
+    std::vector<Vertex*>::iterator vc_itr = std::find(vertices.begin(), vertices.end(), v);
+    if(vc_itr == vertices.end()) 
+        return S_OK;
+
+    Vertex *vn = vc_itr == vertices.begin()   ? *(vertices.end() - 1) : *(vc_itr - 1);
+    Vertex *vp = vc_itr == vertices.end() - 1 ? *(vertices.begin())   : *(vc_itr + 1);
+
+    FVector3 posc = v->getPosition();
+    FVector3 force;
+
+    force += metrics::relativePosition(vp->getPosition(), posc);
+    force += metrics::relativePosition(vn->getPosition(), posc);
+    force *= 2 * lam;
+
+    f[0] += force[0];
+    f[1] += force[1];
+    f[2] += force[2];
+    
+    return S_OK;
+}
---
+++ source/models/vertex/solver/actors/tfEdgeTension.h
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * This file is part of Tissue Forge.
+ * Copyright (c) 2022 T.J. Sego and Tien Comlekoglu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+
+#ifndef _MODELS_VERTEX_SOLVER_ACTORS_TFEDGETENSION_H_
+#define _MODELS_VERTEX_SOLVER_ACTORS_TFEDGETENSION_H_
+
+#include <models/vertex/solver/tfMeshObj.h>
+
+
+namespace TissueForge::models::vertex { 
+
+
+    struct EdgeTension : MeshObjActor {
+
+        float lam;
+
+        EdgeTension(const float &_lam) {
+            lam = _lam;
+        }
+
+        HRESULT energy(MeshObj *source, MeshObj *target, float &e);
+
+        HRESULT force(MeshObj *source, MeshObj *target, float *f);
+    };
+
+}
+
+#endif // _MODELS_VERTEX_SOLVER_ACTORS_TFEDGETENSION_H_
